fea9e4113324c603f8285273539d03b715060087
LiangchengChen <wolfbequiet@gmail.com>
2018-04-10 11:54:45 +0800

    Calculate the aggregate of individual

--- Market_demand.py
+++ Market_demand.py
@@ -1,13 +1,13 @@
+price=input('Please detemine the market prices of [x,y]: ')
+income=input('Please detemine the income of individual: ')
+par=input('Please detemine the parameters of Cobb-Doglous[alpha1,alpha2]: ')
+nper=input('Please determine the number of individuals: ')
+
 import Consumer_class
 
-price=input('Detemine the market prices of [x,y]: ')
-income=input('Detemine the income of individual: ')
-par=input('Detemine the parameters of Cobb-Doglous[alpha1,alpha2]: ')
-nper=input('Determine the number of individuals: ')
-
-A=Consumer(price,income,par)
 Market_D=[0,0]
 for i in range(nper):
-    A=Consumer(price,income,par)
+    A=Consumer_class.Consumer(price,income,par)
     Market_D+=A.utility_max()
 
+print Market_D
