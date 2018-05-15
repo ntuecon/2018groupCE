c9be126320ebe8107a2da021643e2db27f9f437e
David <david.biasi@student.unisg.ch>
2018-05-15 10:31:12 +0800

    More work on technology class

--- CE_economy/technology.py
+++ CE_economy/technology.py
@@ -26,7 +26,7 @@ class Technology(object):
         return 0
 
 
-class CES_tech(Technology):
+class Cobb_Douglas_tech(Technology):
     '''
     This is a classical CES production function as proposed by Solow et al.;
     it assumes that production only relies on two factors, labour and capital, 
@@ -51,17 +51,50 @@ class CES_tech(Technology):
         parameters = dict()
         self.parameters['a'] = float()
         self.parameters['F'] = float()
-        self.parameters['s'] = np.zeros(len(goods))
+        self.parameters['s'] = np.zeros(len(goods)) #goods vector needs to be defined somewhere
         self.parameters['r'] = (self.parameters['s'] - 1) / self.parameters['s']
         self.parameters.update(parameters)
     
     def __call__(self, capital, labour):
         '''
-        Making the CES-function a callable function
+        Making the Cobb-Douglas-function a callable function
         '''
         K = self.capital
         L = self.labour
         q = self.parameters['F'] * (self.parameters['a'] * K^self.parameters['r'])\
         + (1 - self.parameters['a'] * L^self.parameters['r']) ^ (1/self.parameters['r'])
         return q
+
+class CES_tech(Technology):
+    '''
+    This function takes the form of the general CES production function:
+    Q = F * [sum_i=1_n a_i X_i^r](1/r)
+    where Q is the quantity of output for a particular good,
+    F is the total factor productivity,
+    a_i is the share parameter of output for factor i with sum_i=1_n a_i = 1,
+    X_i is the quantity of input for factor i,
+    s = 1 / (1 - r) is the elasticity of substitution
+    '''
     
+    def __init__(self, parameters):
+        '''
+        Constructor
+        '''
+        parameters = dict()
+        self.parameters['a'] = np.array() #of len(factors); maybe list? 
+        self.parameters['F'] = float()
+        self.parameters['s'] = np.zeros(len(goods)) #goods vector needs to be defined
+        self.paremeters['r'] = (self.parameters['s'] - 1) / self.parameters['s']
+        self.parameters.update(parameters)
+    
+    def __call__(self, X):
+        '''
+        Making the general CES function a callable function
+        '''
+        X = np.array()
+        q = 0
+        for i in len(X):
+            q += self.parameters['a'][i] * (X[i] ^ self.parameters['r'])
+        q = q ^ (1/self.parameters['r'])
+        return q
+        
