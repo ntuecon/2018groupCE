c6eabd94451fba3ed993738ebeca57b524ce31d4
David <david.biasi@student.unisg.ch>
2018-06-07 10:08:11 +0800

    none

---
+++ test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 21:20:30 2018
+
+@author: David Biasi
+"""
+class Consumer(object):
+    
+    def __init__(self, alpha, beta):
+        self.alpha = alpha
+        self.beta = beta
+        pass
+
+class Factor(object):
+    
+    def __init__(self, theta):
+        pass
+
+class Good(object):
+    
+    def __init__():
+        pass
+
+class Utility():
+    
+    def __init__(Consumer, sigma, gamma):
+        pass
+    
+    def __call__(h, X, V):
+        pass
+
+class Economy():
+    
+    def __init__(h, X, V):
+        pass
+    
+    def equilibriate():
+        pass
+    
+    def social_welfare():
+        pass
+    
